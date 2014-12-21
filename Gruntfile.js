123e1692e545137f7760f8888f45f8e03172f4f1
David M. Anderson <dma@epsilondelta.us>
2014-12-20 17:32:38 -0800

    Added Gruntfile, invoking load-grunt-config.

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+/*
+  Gruntfile.js
+  David M. Anderson
+
+  NOTE: Using load-grunt-config, so configuration of individual tasks is in
+  separate files under grunt/ .
+*/
+
+module.exports = function( grunt ) {
+    'use strict';
+
+    require( 'load-grunt-config' )( grunt );
+}
