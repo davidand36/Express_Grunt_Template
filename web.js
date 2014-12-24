910d7e9f425f7abd4676ee473717c61230ac76a2
David M. Anderson <dma@epsilondelta.us>
2014-12-23 21:53:05 -0800

    Installed and used compression middleware.

--- web.js
+++ web.js
@@ -7,10 +7,12 @@
 
 var express = require( 'express' );
 var favicon = require( 'serve-favicon' );
+var compression = require( 'compression' );
 
 var app = express();
 
 app.use( favicon( __dirname + '/public/favicon.ico' ) );
+app.use( compression() );
 app.use( express.static( __dirname + '/public' ) );
 
 var port = Number( process.env.PORT || 6203 );
