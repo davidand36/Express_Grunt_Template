54086e71a529871caa72eb382d9ea2fabd343ed6
David M. Anderson <dma@epsilondelta.us>
2014-12-30 13:04:11 -0800

    Installed grunt-contrib-symlink. Set up to link favicon.ico
    Only works after copy:client task has run, since it works in the public/ folder.

---
+++ grunt/symlink.js
@@ -0,0 +1,13 @@
+/*
+  symlink.js
+  David M. Anderson
+
+  Configuration for symlink Grunt task
+*/
+
+module.exports = {
+    favicon: {
+        src: 'public/EpsilonDeltaIcon_16x16.png',
+        dest: 'public/favicon.ico'
+    }
+};
