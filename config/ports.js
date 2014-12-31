5244c061d1fa8b3a1202e635770778c29e614978
David M. Anderson <dma@epsilondelta.us>
2014-12-30 18:07:16 -0800

    Set up node-inspector Grunt task

--- config/ports.js
+++ config/ports.js
@@ -7,5 +7,6 @@
 
 module.exports = {
     server: process.env.PORT || 6203,
-    debugger: process.env.DEBUG_PORT || 16203
+    debugger: process.env.DEBUG_PORT || 16203,
+    inspector: process.env.INSPECTOR_PORT || 26203
 };
---
+++ grunt/node-inspector.js
@@ -0,0 +1,21 @@
+/*
+  node-inspector.js
+  David M. Anderson
+
+  Configuration for node-inspector Grunt task
+*/
+
+var ports = require( '../config/ports' );
+
+module.exports = {
+    dev: {
+        options: {
+            'web-host': 'localhost',
+            'web-port': ports.inspector,
+            'debug-port': ports.debugger,
+            'save-live-edit': true,
+            'no-preload': true,
+            'stack-trace-limit': 10
+        }
+    }
+};
