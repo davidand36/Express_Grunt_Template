3f64c800f837c13b3e2bcf38c7c628575eca7afd
David M. Anderson <dma@epsilondelta.us>
2015-01-12 08:58:52 -0800

    Set up filerev Grunt task for CSS files

---
+++ grunt/filerev.js
@@ -0,0 +1,14 @@
+/*
+  filerev.js
+  David M. Anderson
+
+  Configuration for filerev Grunt task
+  See https://github.com/yeoman/grunt-filerev .
+*/
+
+module.exports = {
+    css: {
+        src: 'public/css/*.min.css',
+        dest: 'public/css/'
+    }
+};
