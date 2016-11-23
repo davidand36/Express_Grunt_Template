5ad7635e763d5ec1568b6eeee7ef7604f9d9f8d0
David M. Anderson <dma@epsilondelta.us>
2016-11-23 15:35:11 -0800

    Added test JS files

---
+++ client/js/test1.js
@@ -0,0 +1,10 @@
+/*
+    test1.js
+
+    Test client script 1
+*/
+
+(function( ) {
+    var message1 = 'Test script 1';
+    console.log( message1 );
+})( );
---
+++ client/js/test2.js
@@ -0,0 +1,10 @@
+/*
+    test2.js
+
+    Test client script 2
+*/
+
+(function( ) {
+    var message2 = 'Test script 2';
+    console.log( message2 );
+})( );
