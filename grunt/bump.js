3d11f4348ee52211b96350deb59b3c3395bfe4c7
David M. Anderson <dma@epsilondelta.us>
2016-11-23 20:38:27 -0800

    Added bump Grunt task

---
+++ grunt/bump.js
@@ -0,0 +1,13 @@
+/*
+    bump.js
+    David M. Anderson
+
+    Configuration for bump Grunt task
+    See https://www.npmjs.com/package/grunt-bump
+*/
+
+module.exports = {
+    options: {
+        push: false
+    }
+};
